f526aaa270a040d61f3bdb240ce67c3e8f352666
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-07-31 14:15:54 -0500

    Initial commit

---
+++ xd/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System;
+using System.Security.Cryptography;
+
+string IVKEY = "R3PR353N";
+string EncryptionK = "rpaSPvIvVLlrcmtzPU9/c67Gkj7yL1S5";
+
+try
+{
+    Console.WriteLine("Ingrese un numero");
+    string numero1 = Console.ReadLine();
+    int intparced;
+    decimal decimalparced;
+
+    if (int.TryParse(numero1, out int parced))
+    {
+        Convert.ToInt32(numero1);
+        byte[] IV = ASCIIEncoding.ASCII.GetBytes(IVKEY);
+        byte[] EncryptionKey = Convert.FromBase64String(EncryptionK);
+        byte[] BUFFER = Encoding.UTF8.GetBytes(numero1);
+        TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+        des.Key = EncryptionKey;
+        des.IV = IV;
+
+        Console.WriteLine($"El numero Encryptado es :" +
+            $" {Convert.ToBase64String(des.CreateEncryptor().TransformFinalBlock(BUFFER, 0, BUFFER.Length))} ");
+    }
+    else if (decimal.TryParse(numero1, out decimalparced))
+    {
+        Console.WriteLine("Usted ingreso un numero decimal y no un numero entero");
+    }
+    else
+    {
+        Console.WriteLine("Usted coloco un string o un numero escrito y no un numero");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocurrio el siguiente error al ejecutar el programa: " + ex.Message);
+    Console.WriteLine("Porfavor comunicarse con TI");
+}
+finally
+{
+    Console.WriteLine("Precione enter para cerrar el programa");
+}
+
---
+++ xd/SimpleAesEncrytion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class SimpleAesEncryption
+{
+    private static readonly string key = "your-32-char-secret-key-here"; // Must be 32 characters for AES-256
+    private static readonly string iv = "your-16-char-IV-here"; // Must be 16 characters for AES
+
+    public static string Encrypt(string plainText)
+    {
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Encoding.UTF8.GetBytes(key);
+            aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+    }
+}
